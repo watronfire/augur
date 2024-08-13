08408580a6beed91e46e93cee28d2ebd552bdbba
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-08-13 16:09:41 -0700

    🚧 Use a WeightsFile class

--- augur/filter/weights_file.py
+++ augur/filter/weights_file.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from textwrap import dedent
-from typing import List
+from typing import List, Optional, Union
 from augur.errors import AugurError
 
 
@@ -13,43 +13,77 @@ class InvalidWeightsFile(AugurError):
         super().__init__(f"Bad weights file {file!r}.\n{error_message}")
 
 
-def read_weights_file(weights_file):
-    weights = pd.read_csv(weights_file, delimiter='\t', comment='#')
+# FIXME: actually use this and figure out how to make the _df attribute mote
+# caller-friendly and not read the file multiple times
+class WeightsFile:
+    """Represents a weights file."""
 
-    if not pd.api.types.is_numeric_dtype(weights[WEIGHTS_COLUMN]):
-        non_numeric_weight_lines = [index + 2 for index in weights[~weights[WEIGHTS_COLUMN].str.isnumeric()].index.tolist()]
-        raise InvalidWeightsFile(weights_file, dedent(f"""\
-            Found non-numeric weights on the following lines: {non_numeric_weight_lines}
-            {WEIGHTS_COLUMN!r} column must be numeric."""))
+    path: str
+    """Path to the file on disk."""
 
-    if any(weights[WEIGHTS_COLUMN] < 0):
-        negative_weight_lines = [index + 2 for index in weights[weights[WEIGHTS_COLUMN] < 0].index.tolist()]
-        raise InvalidWeightsFile(weights_file, dedent(f"""\
-            Found negative weights on the following lines: {negative_weight_lines}
-            {WEIGHTS_COLUMN!r} column must be non-negative."""))
+    _df: pd.DataFrame
 
-    return weights
+    weighted_columns: List[str]
 
+    default_weight: Optional[Union[int, float]]
 
-def get_weighted_columns(weights_file):
-    with open(weights_file) as f:
-        has_rows = False
-        for row in f:
-            has_rows = True
-            if row.startswith('#'):
-                continue
-            columns = row.rstrip().split('\t')
-            break
-    if not has_rows:
-        raise InvalidWeightsFile(weights_file, "File is empty.")
-    columns.remove(WEIGHTS_COLUMN)
-    return columns
+    def __init__(self, path: str, read_dataframe=False):
+        """
+        Parameters
+        ----------
+        path
+            Path of the weights file.
+        read_dataframe
+            Indicate whether to read the file into the dataframe.
+        """
+        self.path = path
 
+        if read_dataframe:
+            self._df = self.read_dataframe()
 
-def get_default_weight(weights: pd.DataFrame, weighted_columns: List[str]):
-    default_weight_values = weights[(weights[weighted_columns] == COLUMN_VALUE_FOR_DEFAULT_WEIGHT).all(axis=1)][WEIGHTS_COLUMN].unique()
+        self.weighted_columns = self._get_weighted_columns()
 
-    if len(default_weight_values) > 1:
-        raise InvalidWeightsFile(f"Multiple default weights were specified: {', '.join(repr(weight) for weight in default_weight_values)}. Only one default weight entry can be accepted.")
-    if len(default_weight_values) == 1:
-        return default_weight_values[0]
+        self.default_weight = self._get_default_weight()
+
+    def read_dataframe(self):
+        weights = pd.read_csv(self.path, delimiter='\t', comment='#')
+
+        if not pd.api.types.is_numeric_dtype(weights[WEIGHTS_COLUMN]):
+            non_numeric_weight_lines = [index + 2 for index in weights[~weights[WEIGHTS_COLUMN].str.isnumeric()].index.tolist()]
+            raise InvalidWeightsFile(self.path, dedent(f"""\
+                Found non-numeric weights on the following lines: {non_numeric_weight_lines}
+                {WEIGHTS_COLUMN!r} column must be numeric."""))
+
+        if any(weights[WEIGHTS_COLUMN] < 0):
+            negative_weight_lines = [index + 2 for index in weights[weights[WEIGHTS_COLUMN] < 0].index.tolist()]
+            raise InvalidWeightsFile(self.path, dedent(f"""\
+                Found negative weights on the following lines: {negative_weight_lines}
+                {WEIGHTS_COLUMN!r} column must be non-negative."""))
+
+        return weights
+
+    def _get_weighted_columns(self):
+        with open(self.path) as f:
+            has_rows = False
+            for row in f:
+                has_rows = True
+                if row.startswith('#'):
+                    continue
+                columns = row.rstrip().split('\t')
+                break
+
+        if not has_rows:
+            raise InvalidWeightsFile(self.path, "File is empty.")
+
+        columns.remove(WEIGHTS_COLUMN)
+
+        return columns
+
+    def _get_default_weight(self):
+        default_weight_values = self._df[(self._df[self.weighted_columns] == COLUMN_VALUE_FOR_DEFAULT_WEIGHT).all(axis=1)][WEIGHTS_COLUMN].unique()
+
+        if len(default_weight_values) > 1:
+            raise InvalidWeightsFile(self.path, f"Multiple default weights were specified: {', '.join(repr(weight) for weight in default_weight_values)}. Only one default weight entry can be accepted.")
+        if len(default_weight_values) == 1:
+            return default_weight_values[0]
+
